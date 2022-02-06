308047215208b08555085d8f73e31f8f9aeece24
Charles Jekel <jekel1@llnl.gov>
2022-02-06 15:00:38 -0800

    switch to first n natural numbers series calculation

--- spdlayers/tools.py
+++ spdlayers/tools.py
@@ -12,6 +12,10 @@ def in_shape_from(output_shape):
     Returns:
         (int): The input shape associated from a
             `[output_shape, output_shape` SPD tensor.
+
+    Notes:
+        This is the sum of the first n natural numbers
+        https://cseweb.ucsd.edu/groups/tatami/handdemos/sum/
     """
-    input_shape = sum([i for i in range(output_shape + 1)])
+    input_shape = output_shape * (output_shape + 1) / 2.
     return input_shape
